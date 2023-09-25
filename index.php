aaa34f6187f05a4fc4da5aea599b592fc4929853
geriard16 <geriard16charitas@gmail.com>
2023-09-25 14:49:15 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, cupiditate laborum nulla veritatis sit accusamus aut voluptate impedit placeat quos adipisci perferendis debitis maxime. Hic placeat quia consequuntur ipsum debitis.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
